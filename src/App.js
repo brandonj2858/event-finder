995be1f3a1b5d1a9701479a935cc91ec73a94732
Brandon James <brandonj2858@gmail.com>
2020-03-09 20:43:27 -0400

    fetch working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [events, setEvents] = useState("")
+
+  useEffect(() => {
+
+      const xmlInfo = fetch('http://api.eventful.com/rest/events/search?&app_key=4qgTBpXfK99TR8Qk&keywords=books&location=San+Diego&date=Future')
+        .then(res => res.text())
+        .then(data => setEvents(data))
+
+  }
+
+)
+
+  const handleClick = (evt) => {
+    const str = events
+    console.log(str)
+    const parser = new DOMParser()
+    const allEvents = (console.log(parser.parseFromString(str, "text/xml").getElementsByTagName('event')))
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navBar">
+      <ul className="NavLinksUl">
+        <li className="navLinkLI"><a className="navLink" href="#home">Artist</a></li>
+        <li className="navLinkLI"><a className="navLink" href="#news">Venue</a></li>
+        <li className="navLinkLI"><a className="navLink" href="#contact">Genre</a></li>
+        <li className="navLinkLI"><a className="navLink" href="#about">About</a></li>
+      </ul>
+
+      </div>
+      <div onClick={handleClick} className="mainContainer">
+
+
+      </div>
+
     </div>
   );
 }
